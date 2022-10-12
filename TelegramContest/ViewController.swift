73d99ce5a07b0e6dbe4c6786ef588287969dff0e
aurocheg <aurocheg@gmail.com>
2022-10-12 15:10:13 +0300

    Initial Commit

---
+++ TelegramContest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelegramContest
+//
+//  Created by Aurocheg on 12.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
